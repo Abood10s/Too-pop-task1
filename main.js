2ad1d5cb12bb9d26cac425a4fc894ae7d951bbf9
Abood10s <saafeenabood@gmail.com>
2023-07-03 12:57:19 +0300

    adding responsive styles to navbar and adding accordions functionality

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const plusIcon = document.getElementById("plus");
-const content = document.getElementById("content");
+const menu = document.querySelector(".menu");
+const close = document.getElementById("close");
+const actions = document.querySelector(".actions");
+const links = document.querySelector(".links");
+const showAcc = document.querySelectorAll(".showAcc");
+const contentAcc = document.querySelectorAll(".content");
 
-plusIcon.addEventListener("click", function () {
-  content.style.height = content.style.height === "0px" ? "auto" : "0px";
-  plusIcon.innerHTML =
-    content.style.height === "0px"
-      ? `<i class="fa-regular fa-plus"></i>`
-      : `<i class="fa-solid fa-minus"></i>`;
+menu.addEventListener("click", function () {
+  links.classList.toggle("slide");
+});
+
+showAcc.forEach(function (acc, index) {
+  acc.addEventListener("click", function () {
+    contentAcc[index].classList.toggle("show");
+    const icon = acc.querySelector("i");
+    icon.classList.toggle("fa-minus");
+    icon.classList.toggle("fa-plus");
+    icon.classList.replace("fa-solid", "fa-regular");
+    icon.classList.replace("fa-regular", "fa-solid");
+  });
 });
